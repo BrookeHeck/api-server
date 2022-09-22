f6a21c399b838eef18d88136976de5ffaa170b0b
Brooke Heck <b19heck@gmail.com>
2022-09-22 21:57:02 +0000

    add error handling

--- src/server.js
+++ src/server.js
@@ -3,15 +3,15 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-// const logger = require('./middleware/logger');
+const logger = require('./middleware/logger');
 const food = require('./routes/food');
 const clothes = require('./routes/clothes');
-// const send404 = require('./error-handlers/404');
-// const send500 = require('./error-handlers/500');
+const send404 = require('./error-handlers/404');
+const send500 = require('./error-handlers/500');
 
 app.use(cors());
 app.use(express.json());
-// app.use(logger);
+app.use(logger);
 
 app.get('/', (request, response) => {
   response.status(200).send('My Server');
@@ -20,7 +20,7 @@ app.get('/', (request, response) => {
 app.use(food);
 app.use(clothes);
 
-// app.get('*', send404);
-// app.use(send500);
+app.get('*', send404);
+app.use(send500);
 
 module.exports = app;
